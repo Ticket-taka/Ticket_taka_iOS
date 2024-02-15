b06877f37e951dd6bc5503bcb32eef8624ef5488
oyslucy <oyslucy@naver.com>
2024-02-16 05:34:33 +0900

    add api

--- home/Sources/Data/Model/Patch.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Patch.swift
-//  home
-//
-//  Created by 오연서 on 2/13/24.
-//
-
-import Foundation
--- home/Sources/Data/Model/Post.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Post.swift
-//  home
-//
-//  Created by 오연서 on 2/13/24.
-//
-
-import Foundation
